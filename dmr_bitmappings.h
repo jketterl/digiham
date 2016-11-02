963441cf6a67c1fe00e91cea405f84054ddf57ac
Jakob Ketterl <jakob.ketterl@gmx.de>
2016-11-02 17:24:13 +0000

    dmr voice extraction and de-interleaving

---
+++ dmr_bitmappings.h
@@ -0,0 +1,23 @@
+#include <stdint.h>
+
+// there seems to be a system here, at least to some extent. but parts of it are pretty arbitrary and would result in pretty unintelligible condions, so i'll leave it as a constant here.
+uint8_t voice_mapping[72] = {
+    23, 5,  34, 51,
+    22, 4,  33, 50,
+    21, 3,  32, 49,
+    20, 2,  31, 48,
+    19, 1,  30, 85,
+    18, 0,  29, 84,
+    17, 46, 28, 83,
+    16, 45, 27, 82,
+    15, 44, 26, 81,
+    14, 43, 25, 80,
+    13, 42, 24, 79,
+    12, 41, 58, 78,
+    11, 40, 57, 77,
+    10, 39, 56, 76,
+    9,  38, 55, 75,
+    8,  37, 54, 74,
+    7,  36, 53, 73,
+    6,  35, 52, 72
+};
--- dmr_decoder.c
+++ dmr_decoder.c
@@ -2,6 +2,8 @@
 #include <unistd.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <string.h>
+#include "dmr_bitmappings.h"
 
 #define BUF_SIZE 128
 #define RINGBUFFER_SIZE 1024
@@ -13,41 +15,104 @@ int ringbuffer_read_pos = 0;
 
 #define SYNC_SIZE 24
 
+#define SYNCTYPE_UNKNOWN 0
+#define SYNCTYPE_DATA 1
+#define SYNCTYPE_VOICE 2
+
+int synctypes[2] = { SYNCTYPE_UNKNOWN, SYNCTYPE_UNKNOWN };
+
 uint8_t dmr_bs_data_sync[] =  { 3,1,3,3,3,3,1,1,1,3,3,1,1,3,1,1,3,1,3,3,1,1,3,1 };
 uint8_t dmr_bs_voice_sync[] = { 1,3,1,1,1,1,3,3,3,1,1,3,3,1,3,3,1,3,1,1,3,3,1,3 };
 uint8_t dmr_ms_data_sync[] =  { 3,1,1,1,3,1,1,3,3,3,1,3,1,3,3,3,3,1,1,3,1,1,1,3 };
 uint8_t dmr_ms_voice_sync[] = { 1,3,3,3,1,3,3,1,1,1,3,1,3,1,1,1,1,3,3,1,3,3,3,1 };
 
+
+void DumpHex(const void* data, size_t size) {
+    char ascii[17];
+    size_t i, j;
+    ascii[16] = '\0';
+    for (i = 0; i < size; ++i) {
+        fprintf(stderr, "%02X ", ((unsigned char*)data)[i]);
+        if (((unsigned char*)data)[i] >= ' ' && ((unsigned char*)data)[i] <= '~') {
+            ascii[i % 16] = ((unsigned char*)data)[i];
+        } else {
+            ascii[i % 16] = '.';
+        }
+        if ((i+1) % 8 == 0 || i+1 == size) {
+            fprintf(stderr, " ");
+            if ((i+1) % 16 == 0) {
+                fprintf(stderr, "|  %s \n", ascii);
+            } else if (i+1 == size) {
+                ascii[(i+1) % 16] = '\0';
+                if ((i+1) % 16 <= 8) {
+                    fprintf(stderr, " ");
+                }
+                for (j = (i+1) % 16; j < 16; ++j) {
+                    fprintf(stderr, "   ");
+                }
+                fprintf(stderr, "|  %s \n", ascii);
+            }
+        }
+    }
+}
+
+
+// modulo that will respect the sign
 unsigned int mod(int n, int x) { return ((n%x)+x)%x; }
 
 int ringbuffer_bytes() {
     return mod(ringbuffer_write_pos - ringbuffer_read_pos, RINGBUFFER_SIZE);
 }
 
-bool is_sync(uint8_t potential_sync[24]) {
+int get_synctype(uint8_t potential_sync[24]) {
     if (memcmp(potential_sync, dmr_bs_data_sync, SYNC_SIZE) == 0) {
         fprintf(stderr, "found a bs data sync at pos %i\n", ringbuffer_read_pos);
-        return true;
+        return SYNCTYPE_DATA;
     }
     if (memcmp(potential_sync, dmr_bs_voice_sync, SYNC_SIZE) == 0) {
         fprintf(stderr, "found a bs voice sync at pos %i\n", ringbuffer_read_pos);
-        return true;
+        return SYNCTYPE_VOICE;
     }
     if (memcmp(potential_sync, dmr_ms_data_sync, SYNC_SIZE) == 0) {
         fprintf(stderr, "found a ms data sync at pos %i\n", ringbuffer_read_pos);
-        return true;
+        return SYNCTYPE_DATA;
     }
     if (memcmp(potential_sync, dmr_ms_voice_sync, SYNC_SIZE) == 0) {
         fprintf(stderr, "found a ms voice sync at pos %i\n", ringbuffer_read_pos);
-        return true;
+        return SYNCTYPE_VOICE;
     }
-    return false;
+    return SYNCTYPE_UNKNOWN;
+}
+
+void deinterleave_voice_payload(uint8_t payload[27], uint8_t result[36]) {
+    memset(result, 0, 36);
+    int frame = 0;
+    int input_bit = 0;
+
+    // 27 bytes of payload contain 3 vocoder frames à 72 bits
+    for (frame = 0; frame < 3; frame++) {
+        for (input_bit = 0; input_bit < 72; input_bit++) {
+            int input_position = input_bit / 8;
+            int input_shift = 7 - input_bit % 8;
+
+            int output_bit = voice_mapping[input_bit];
+            int output_position = output_bit / 8;
+            int output_shift = 7 - output_bit % 8;
+
+            int x = (payload[frame * 9 + input_position] >> input_shift) & 1;
+
+            // output will be blown up to 96 bits per frame
+            result[frame * 12 + output_position] |= x << output_shift;
+        }
+    }
 }
 
 void main() {
     int r = 0;
     bool sync = false;
     int sync_missing = 0;
+    FILE *fp = fopen("/tmp/payload.data", "w");
+    fputs(".amb", fp);
     while ((r = fread(buf, 1, BUF_SIZE, stdin)) > 0) {
         int i;
         for (i = 0; i < r; i++) {
@@ -66,7 +131,7 @@ void main() {
             int k;
             for (k = 0; k < SYNC_SIZE; k++) potential_sync[k] = ringbuffer[(ringbuffer_read_pos + k) % RINGBUFFER_SIZE];
 
-            if (is_sync(potential_sync)) {
+            if (get_synctype(potential_sync) != SYNCTYPE_UNKNOWN) {
                 sync = true; sync_missing = 0;
                 break;
             }
@@ -80,7 +145,8 @@ void main() {
             uint8_t potential_sync[SYNC_SIZE];
             for (k = 0; k < SYNC_SIZE; k++) potential_sync[k] = ringbuffer[(ringbuffer_read_pos + k) % RINGBUFFER_SIZE];
 
-            if (is_sync(potential_sync)) {
+            int synctype = get_synctype(potential_sync);
+            if (synctype != SYNCTYPE_UNKNOWN) {
                 sync_missing = 0;
             } else {
                 fprintf(stderr, "going to %i without sync\n", ringbuffer_read_pos);
@@ -90,15 +156,18 @@ void main() {
             if (sync_missing >= 12) {
                 fprintf(stderr, "lost sync at %i\n", ringbuffer_read_pos);
                 sync = false;
+                synctypes[0] = SYNCTYPE_UNKNOWN; synctypes[1] = SYNCTYPE_UNKNOWN;
                 break;
             }
 
             uint8_t tact = 0;
-            // CACH Interleaving
+            // CACH starts 54 (payload area) + 12 (cach itself) dibits before the sync
+            int cach_start = ringbuffer_read_pos - (54 + 12);
+            // extract TACT bits from CACH
             for (k = 0; k < 7; k++) {
                 int pos = k * 2;
                 if (pos > 8) pos--;
-                tact = (tact << 1) | ((ringbuffer[(ringbuffer_read_pos + 24 + 54 + pos) % RINGBUFFER_SIZE] & 2) >> 1);
+                tact = (tact << 1) | ((ringbuffer[(cach_start + pos) % RINGBUFFER_SIZE] & 2) >> 1);
             }
 
             // HAMMING checksum
@@ -118,8 +187,36 @@ void main() {
             }
             */
 
-            fprintf(stderr, "  slot: %i busy: %i, lcss: %i\n", (tact & 32) >> 5, (tact & 64) >> 6, (tact & 24) >> 3);
+            uint8_t slot = (tact & 32) >> 5;
 
+            if (synctype != SYNCTYPE_UNKNOWN) synctypes[slot] = synctype;
+
+            fprintf(stderr, "  slot: %i busy: %i, lcss: %i\n", slot, (tact & 64) >> 6, (tact & 24) >> 3);
+
+            // extract payload data
+            uint8_t payload[27] = {0};
+            // first half
+            int payload_start = ringbuffer_read_pos - 54;
+            for (k = 0; k < 54; k++) {
+                payload[k / 4] |= (ringbuffer[(payload_start + k) % RINGBUFFER_SIZE] & 3) << (6 - 2 * (k % 4));
+            }
+
+            // second half
+            payload_start = ringbuffer_read_pos + SYNC_SIZE;
+            for (k = 0; k < 54; k++) {
+                payload[(k + 54) / 4] |= (ringbuffer[(payload_start + k) % RINGBUFFER_SIZE]) << (6 - 2 * ((k + 54) % 4));
+            }
+
+            if (synctypes[slot] == SYNCTYPE_VOICE) {
+                //DumpHex(payload, 27);
+                uint8_t deinterleaved[36];
+                deinterleave_voice_payload(payload, deinterleaved);
+                //DumpHex(&deinterleaved, 36);
+                fwrite(deinterleaved, 1, 36, fp);
+                fflush(fp);
+            }
+
+            // advance to the next frame. as long as we have sync, we know where the next frame begins
             ringbuffer_read_pos = mod(ringbuffer_read_pos + 144, RINGBUFFER_SIZE);
         }
     }
