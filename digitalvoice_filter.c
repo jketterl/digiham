6a26f3fa025098707531af2b8b311244f869141c
Jakob Ketterl <jakob.ketterl@gmx.de>
2019-05-25 01:42:41 +0200

    add new audio output filter, according to some spec on the internet

---
+++ digitalvoice_filter.c
@@ -0,0 +1,38 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <limits.h>
+
+#define NZEROS 10
+#define NPOLES 10
+#define GAIN   2.823549227e+00
+
+float xv[NZEROS+1], yv[NPOLES+1];
+
+float filter(float sample) {
+    xv[0] = xv[1]; xv[1] = xv[2]; xv[2] = xv[3]; xv[3] = xv[4]; xv[4] = xv[5]; xv[5] = xv[6]; xv[6] = xv[7]; xv[7] = xv[8]; xv[8] = xv[9]; xv[9] = xv[10];
+    xv[10] = sample / GAIN;
+    yv[0] = yv[1]; yv[1] = yv[2]; yv[2] = yv[3]; yv[3] = yv[4]; yv[4] = yv[5]; yv[5] = yv[6]; yv[6] = yv[7]; yv[7] = yv[8]; yv[8] = yv[9]; yv[9] = yv[10];
+    yv[10] =   (xv[10] - xv[0]) + 5 * (xv[2] - xv[8]) + 10 * (xv[6] - xv[4])
+                 + (  0.1254306222 * yv[0]) + (  0.1285714097 * yv[1])
+                 + ( -0.8106454980 * yv[2]) + ( -0.7664515771 * yv[3])
+                 + (  2.1846187758 * yv[4]) + (  1.8106678608 * yv[5])
+                 + ( -3.1465011600 * yv[6]) + ( -2.0391991609 * yv[7])
+                 + (  2.4873968618 * yv[8]) + (  1.0249072542 * yv[9]);
+    return yv[10];
+}
+
+#define BUF_SIZE 256
+short buf[BUF_SIZE];
+int r = 0;
+
+int main() {
+    while ((r = fread(buf, 2, BUF_SIZE, stdin)) > 0) {
+        int i;
+        for (i = 0; i < r; i++) {
+            buf[i] = (short) (filter((float)buf[i] / SHRT_MAX) * SHRT_MAX);
+        }
+        fwrite(buf, 2, r, stdout);
+    }
+
+    return 0;
+}
