b1de073648fcfe1850510aadac1cf36a4c65fbea
Jakob Ketterl <jakob.ketterl@gmx.de>
2019-05-28 23:10:13 +0200

    it's either sync or emb

--- dmr_decoder.c
+++ dmr_decoder.c
@@ -317,44 +317,46 @@ int main(int argc, char** argv) {
 
             bool emb_present = false;
             uint8_t emb_data = 0;
-            short emb = 0;
-
-            // try to decode as embedded signalling
-            for (k = 0; k < 4; k++) {
-                emb = (emb << 2) | ringbuffer[(ringbuffer_read_pos + k) % RINGBUFFER_SIZE];
-            }
-            for (k = 0; k < 4; k++) {
-                emb = (emb << 2) | ringbuffer[(ringbuffer_read_pos + 20 + k) % RINGBUFFER_SIZE];
-            }
-
-            // check emb header
-            short parity = 0;
-            uint8_t data = (emb & 0xFE00) >> 9;
-            for (k = 0; k < 9; k++) {
-                int bit = 0, l;
-                for (l = 0; l < 7; l++) {
-                    if ((emb_qr_matrix[k] >> l) & 1) {
-                        bit ^= ((data >> l) & 1);
-                    }
-                }
-
-                parity = (parity << 1) | (bit & 1);
-            }
-
-            short parity_input = emb & 0x01FF;
-            if (parity == parity_input) {
-                emb_present = true;
-                emb_data = data;
-            } else {
-                // TODO correct with the parity information?
-                emb_present = false;
-            }
-
-            // if the EMB decoded correctly, that counts towards the sync :)
-            if (synctype != SYNCTYPE_UNKNOWN || emb_present) {
+            // embedded signaling takes the place of the sync
+            if (synctype != SYNCTYPE_UNKNOWN) {
                 sync_missing = 0;
             } else {
-                sync_missing ++;
+                short emb = 0;
+
+                // try to decode as embedded signalling
+                for (k = 0; k < 4; k++) {
+                    emb = (emb << 2) | ringbuffer[(ringbuffer_read_pos + k) % RINGBUFFER_SIZE];
+                }
+                for (k = 0; k < 4; k++) {
+                    emb = (emb << 2) | ringbuffer[(ringbuffer_read_pos + 20 + k) % RINGBUFFER_SIZE];
+                }
+
+                // check emb header
+                short parity = 0;
+                uint8_t data = (emb & 0xFE00) >> 9;
+                for (k = 0; k < 9; k++) {
+                    int bit = 0, l;
+                    for (l = 0; l < 7; l++) {
+                        if ((emb_qr_matrix[k] >> l) & 1) {
+                            bit ^= ((data >> l) & 1);
+                        }
+                    }
+
+                    parity = (parity << 1) | (bit & 1);
+                }
+
+                short parity_input = emb & 0x01FF;
+                if (parity == parity_input) {
+                    // if the EMB decoded correctly, that counts towards the sync :)
+                    sync_missing = 0;
+                    emb_present = true;
+                    emb_data = data;
+                } else {
+                    sync_missing ++;
+                    // TODO correct with the parity information?
+                    emb_present = false;
+                }
+
             }
 
             if (sync_missing >= 12) {
