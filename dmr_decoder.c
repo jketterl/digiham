1608ec062275b88e886c0a00e95d98886ead129c
Jakob Ketterl <jakob.ketterl@gmx.de>
2019-05-30 14:33:31 +0200

    implement FLC checksum

--- dmr_decoder.c
+++ dmr_decoder.c
@@ -206,11 +206,50 @@ void decode_embedded_data(slot) {
         }
     }
 
-    for (i = 0; i < 8; i++) {
+    for (i = 0; i < 7; i++) {
         if (!hamming_16_11(&decode_matrix[i])) {
             fprintf(stderr, "incorrectable error in FLC (position %i)\n", i);
             return;
         }
+    }
+
+    // column parity check as described in B.2.1
+    uint16_t parity = 0;
+    for (i = 0; i < 8; i++) {
+        parity ^= decode_matrix[i];
+    }
+    if (parity != 0) {
+        fprintf(stderr, "FLC parity error: %i\n", parity);
+        return;
+    }
+
+    // de-interleave according to etsi B.2.1
+    uint8_t lc[9];
+    lc[0] =  (decode_matrix[0] & 0b1111111100000000) >> 8;
+    lc[1] =  (decode_matrix[0] & 0b0000000011100000)       | ((decode_matrix[1] & 0b1111100000000000) >> 11);
+    lc[2] = ((decode_matrix[1] & 0b0000011111100000) >> 3) | ((decode_matrix[2] & 0b1100000000000000) >> 14);
+    lc[3] =  (decode_matrix[2] & 0b0011111111000000) >> 6;
+    lc[4] =  (decode_matrix[3] & 0b1111111100000000) >> 8;
+    lc[5] =  (decode_matrix[3] & 0b0000000011000000)       | ((decode_matrix[4] & 0b1111110000000000) >> 10);
+    lc[6] = ((decode_matrix[4] & 0b0000001111000000) >> 2) | ((decode_matrix[5] & 0b1111000000000000) >> 12);
+    lc[7] = ((decode_matrix[5] & 0b0000111111000000) >> 4) | ((decode_matrix[6] & 0b1100000000000000) >> 14);
+    lc[8] =  (decode_matrix[6] & 0b0011111111000000) >> 6;
+
+    // checksum calculation according to etsi B.2.1 and B.3.11
+    uint16_t checksum = 0;
+    for (i = 0; i < 9; i++) {
+        checksum += lc[i];
+    }
+    uint8_t checksum_mod = checksum % 31;
+
+    uint8_t received_checksum = 0;
+    for (i = 0; i < 5; i++) {
+        received_checksum |= (decode_matrix[i+2] & 0b0000000000100000) >> (i + 1);
+    }
+
+    if (checksum_mod != received_checksum) {
+        fprintf(stderr, "FLC checksum error; checksum: %i, received checksum: %i\n", checksum_mod, received_checksum);
+        return;
     }
 
     uint8_t flc_opcode = (decode_matrix[0] & 0x3F00) >> 8;
