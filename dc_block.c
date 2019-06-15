4f6a5135ffe16624aa0bdef93cef69a64f8e1340
Jakob Ketterl <jakob.ketterl@gmx.de>
2019-06-15 12:51:50 +0200

    usage and version

--- dc_block.c
+++ dc_block.c
@@ -1,5 +1,7 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <getopt.h>
+#include "version.h"
 
 #define BUF_SIZE 256
 float buf[BUF_SIZE];
@@ -7,7 +9,35 @@ int r = 0;
 #define R 0.995f
 #define GAIN (1 + R) / 2
 
-int main() {
+void print_usage() {
+    fprintf(stderr,
+        "dc_block version %s\n\n"
+        "Usage: dc_block [options]\n\n"
+        "Available options:\n"
+        " -h, --help      show this message\n"
+        " -v, --version   print version and exit\n",
+        VERSION
+    );
+}
+
+int main(int argc, char** argv) {
+    int c;
+    static struct option long_options[] = {
+        {"help", no_argument, NULL, 'h'},
+        {"version", no_argument, NULL, 'v'},
+        { NULL, 0, NULL, 0 }
+    };
+    while ((c = getopt_long(argc, argv, "hv", long_options, NULL)) != -1) {
+        switch (c) {
+            case 'v':
+                print_version();
+                return 0;
+            case 'h':
+                print_usage();
+                return 0;
+        }
+    }
+
     float xm1 = 0;
     float ym1 = 0;
     while ((r = fread(buf, 4, BUF_SIZE, stdin)) > 0) {
