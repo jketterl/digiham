e7c4c82ee92db8795986d4466915f6456edff7c0
Jakob Ketterl <jakob.ketterl@gmx.de>
2021-06-02 13:40:20 +0200

    add new radios

--- src/ysf_decoder/radio_types.h
+++ src/ysf_decoder/radio_types.h
@@ -6,13 +6,16 @@ struct radio {
 };
 
 static struct radio radio_types[] = {
+    {0x20, "DR-2X"},
     {0x24, "FT-1D"},
     {0x25, "FTM-400D"},
     {0x26, "DR-1X"},
     {0x27, "FT-991"},
     {0x28, "FT-2D"},
     {0x29, "FTM-100D"},
-    {0x2B, "FT-70D"}
+    {0x2B, "FT-70D"},
+    {0x30, "FT-3D"},
+    {0x31, "FTM-300D"}
 };
 
 char* get_radio_type(uint8_t id);
