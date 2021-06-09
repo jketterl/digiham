578353ee869349ab2bee3611ec9617126ba12710
Jakob Ketterl <jakob.ketterl@gmx.de>
2021-06-09 13:55:16 +0200

    implement sync phase

--- src/nxdn_decoder/nxdn_phase.cpp
+++ src/nxdn_decoder/nxdn_phase.cpp
@@ -1,7 +1,30 @@
 #include "nxdn_phase.hpp"
 
+#include <iostream>
+
+extern "C" {
+#include "hamming_distance.h"
+}
+
 using namespace Digiham::Nxdn;
 
+// -3. +1, -3, +3, -3, -3, +3, +3, -1, +3
+const uint8_t SyncPhase::frameSync[SYNC_SIZE] = { 3, 0, 3, 1, 3, 3, 1, 1, 2, 1 };
+
 Digiham::Phase* SyncPhase::process(Ringbuffer* data, size_t& read_pos) {
-    return nullptr;
+    //std::cerr << "scanning ringbuffer at " << read_pos << "\n";
+
+    uint8_t potential_sync[SYNC_SIZE];
+    data->read((char*) potential_sync, read_pos, SYNC_SIZE);
+
+    if (hamming_distance(potential_sync, (uint8_t*) frameSync, SYNC_SIZE) <= 2) {
+        std::cerr << "found a frame sinc at pos " << read_pos << "\n";
+        data->advance(read_pos, SYNC_SIZE);
+        // return new HeaderPhase();
+    }
+
+    // as long as we don't find any sync, move ahead, bit by bit
+    data->advance(read_pos, 1);
+    // tell decoder that we'll continue
+    return this;
 }
--- src/nxdn_decoder/nxdn_phase.hpp
+++ src/nxdn_decoder/nxdn_phase.hpp
@@ -3,12 +3,16 @@
 #include "phase.hpp"
 #include "ringbuffer.hpp"
 
+#define SYNC_SIZE 10
+
 namespace Digiham::Nxdn {
 
     class SyncPhase: public Digiham::Phase {
         public:
             int getRequiredData() override { return 10; }
             Digiham::Phase* process(Ringbuffer* data, size_t& read_pos) override;
+        private:
+            static const uint8_t frameSync[SYNC_SIZE];
     };
 
 }
