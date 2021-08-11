c94efb4b97ed6fb79a575ea7f7abb430b9d8d5b2
Jakob Ketterl <jakob.ketterl@gmx.de>
2021-08-11 12:08:38 +0200

    de-allocate properly

--- src/mbe_synthesizer/mbe_synthesizer_cli.cpp
+++ src/mbe_synthesizer/mbe_synthesizer_cli.cpp
@@ -2,5 +2,7 @@
 
 int main (int argc, char** argv) {
     CodecServer::Cli* cli = new CodecServer::Cli();
-    return cli->main(argc, argv);
+    int rc = cli->main(argc, argv);
+    delete cli;
+    return rc;
 }
